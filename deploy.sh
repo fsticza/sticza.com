695a7e78c8cb9f119e1c62927bb97ec4dab5fdb0
Ferenc Sticza <ferenc.sticza@gmail.com>
2024-05-16 08:58:08 +0200

    fix: deploy config

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,14 @@ npm run generate:gh-pages
 
 # navigate into the build output directory
 cd dist
-
+cp ../CNAME .
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git config user.name "Ferenc Sticza"
 git config user.email "ferenc.sticza@gmail.com"
+git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
@@ -22,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:fsticza/fsticza.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fsticza/sticza.com.git master:gh-pages
+ git push -f -u git@github.com:fsticza/sticza.com.git gh-pages
 
 cd -
